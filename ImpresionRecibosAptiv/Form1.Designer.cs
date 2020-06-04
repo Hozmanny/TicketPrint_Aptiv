6233b74438e683a90b39033f24184c1c0e0e123e
Hozmanny <hozmanny.garcia@gmail.com>
2020-06-04 13:47:03 -0600

    Add project files.

---
+++ ImpresionRecibosAptiv/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace ImpresionRecibosAptiv
+{
+    partial class FormBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbGafete = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.label1.Location = new System.Drawing.Point(32, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(613, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Impresión de recibos de nomina.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto Lt", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.label2.Location = new System.Drawing.Point(33, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(833, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Escanea el código de tu gafete y presiona Buscar.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.label3.Location = new System.Drawing.Point(32, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gafete:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Roboto", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.label4.Location = new System.Drawing.Point(32, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 46);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nombre:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Roboto Lt", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.label5.Location = new System.Drawing.Point(33, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(794, 37);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Si es correcta tu información presiona el botón de imprimir,";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Roboto Lt", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.label6.Location = new System.Drawing.Point(33, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(605, 37);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "de lo contrario presiona el botón de cancelar.";
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(172)))), ((int)(((byte)(158)))));
+            this.btnImprimir.Font = new System.Drawing.Font("Roboto", 24F);
+            this.btnImprimir.ForeColor = System.Drawing.Color.White;
+            this.btnImprimir.Location = new System.Drawing.Point(40, 458);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(380, 92);
+            this.btnImprimir.TabIndex = 6;
+            this.btnImprimir.Text = "IMPRIMIR RECIBO";
+            this.btnImprimir.UseVisualStyleBackColor = false;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.btnCancelar.Font = new System.Drawing.Font("Roboto", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(715, 458);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(364, 92);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Roboto Th", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.label7.Location = new System.Drawing.Point(33, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1049, 37);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "_________________________________________________________________________________" +
+    "_____";
+            // 
+            // tbGafete
+            // 
+            this.tbGafete.Font = new System.Drawing.Font("Roboto", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbGafete.Location = new System.Drawing.Point(207, 164);
+            this.tbGafete.Name = "tbGafete";
+            this.tbGafete.Size = new System.Drawing.Size(250, 52);
+            this.tbGafete.TabIndex = 9;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Enabled = false;
+            this.tbNombre.Font = new System.Drawing.Font("Roboto", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNombre.Location = new System.Drawing.Point(207, 273);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(653, 52);
+            this.tbNombre.TabIndex = 10;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(198)))), ((int)(((byte)(235)))));
+            this.btnBuscar.Font = new System.Drawing.Font("Roboto", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Location = new System.Drawing.Point(715, 121);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(364, 92);
+            this.btnBuscar.TabIndex = 11;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ImpresionRecibosAptiv.Properties.Resources.aptiv_logo_color_rgb;
+            this.pictureBox1.Location = new System.Drawing.Point(988, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(239)))), ((int)(((byte)(237)))));
+            this.ClientSize = new System.Drawing.Size(1119, 613);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbGafete);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormBase";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormBase_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbGafete;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ ImpresionRecibosAptiv/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Drawing.Printing;
+using System.Diagnostics;
+
+namespace ImpresionRecibosAptiv
+{
+    public partial class FormBase : Form
+    {
+        public FormBase()
+        {
+            InitializeComponent();
+
+        }
+
+        private void FormBase_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        string result, bbva;
+        public void btnImprimir_Click(object sender, EventArgs e)
+        {
+            if (bbva == null)
+            {
+                MessageBox.Show("Busca primero tu numero de gafete", "Ingresar gafete", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                //System.Diagnostics.Process.Start(bbva);
+
+                SendToPrinter(bbva);
+                tbNombre.Clear();
+                tbGafete.Clear();
+                bbva = null;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            tbNombre.Clear();
+            tbGafete.Clear();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (tbGafete.Text == "")
+            {
+                MessageBox.Show("Escanea o ingresa tu numero de gafete","Falta de gafete", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                bbva = Buscar(tbGafete.Text);
+                if (bbva == "No se encontro nada")
+                {
+                    MessageBox.Show("No se se encuentra recibo", "Busqueda sin exito", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    tbGafete.Clear();
+                    tbNombre.Clear();
+                }
+            }
+        }
+
+        public string Buscar(string gafete)
+        {
+            string vScan = gafete;
+            string[] dirs = Directory.GetFiles(@"C:\Users\MANNY\Documents\Aptiv_Kiosco\", "*.xml");
+            string attrVal, nombre;
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(dirs[i]);
+                XmlNodeList elemList = doc.GetElementsByTagName("nomina12:Receptor");
+                XmlNodeList elemList2 = doc.GetElementsByTagName("cfdi:Receptor");
+                attrVal = elemList[0].Attributes["NumEmpleado"].Value;
+                nombre = elemList2[0].Attributes["Nombre"].Value;
+                if (vScan == attrVal)
+                {
+                    tbNombre.Text = nombre;
+
+                    result = Path.ChangeExtension(dirs[i], ".pdf");
+                    return result;
+                    //System.Diagnostics.Process.Start(result);
+
+                    //break;
+                }
+            }
+            return "No se encontro nada";
+        }
+
+        private void SendToPrinter(string patho)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Verb = "print";
+            info.FileName = patho;
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process p = new Process();
+            p.StartInfo = info;
+            p.Start();
+
+            p.WaitForInputIdle();
+            System.Threading.Thread.Sleep(3000);
+            if (false == p.CloseMainWindow())
+                p.Kill();
+        }
+    }
+}
